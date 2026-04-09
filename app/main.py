dee68915dc883022869d468a070f8452b6838fe8
Burak Arabacioglu <burak.arabacioglu.work@gmail.com>
2026-04-09 15:18:45 +0300

    Implement complete User CRUD lifecycle
    - Added POST for user registration with Pydantic validation
    - Added GET for individual and bulk user retrieval
    - Implemented dynamic PUT for flexible user updates
    - Added DELETE with 404 error handling for missing records"

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Path, HTTPException
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+from starlette import status
+
 from app.db.database import engine, SessionLocal
-from app.models.users import Base
+from app.models.users import Base, User
+from sqlalchemy.orm import Session
 
 # Create the files before the app starts.
 Base.metadata.create_all(engine)
 app = FastAPI()
+
+class UserRegister(BaseModel):
+    first_name: str = Field(min_length=1, max_length=15)
+    last_name: str = Field(min_length=1, max_length=30)
+    email: str
+    password: str
+    bodyfat: float
+
 
 def get_db():
     db = SessionLocal()
@@ -12,6 +26,47 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/read-all")
+async def read_all(db:db_dependency):
+    return db.query(User).all()
+
+@app.get("/get_by_id/{user_id}", status_code=status.HTTP_200_OK)
+async def get_by_id(user_id: int = Path(gt=0), db:Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is not None:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@app.post("/create-user", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserRegister, db:db_dependency):
+    user = User(**user.model_dump())
+    db.add(user)
+    db.commit()
+    return user
+
+@app.put("/update-user/{user_id}", status_code=status.HTTP_200_OK)
+async def update_user(user_request: UserRegister, db:db_dependency, user_id: int = Path(gt=0)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    for key,value in user_request.model_dump(exclude_unset=True).items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.delete("/delete-user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(db:db_dependency, user_id: int = Path(gt=0)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    db.delete(db_user)
+    db.commit()
+
 
 @app.get("/")
 async def root():
